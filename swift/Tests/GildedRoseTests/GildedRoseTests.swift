93fa1cf0e244d7966e0196e13dc07c21aa83f5b7
Daniel Tartaglia <danielt1263@gmail.com>
2024-07-14 13:31:56 -0400

    Duplicate GildedRose class. Create property test harness.

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -2,10 +2,74 @@
 import XCTest
 
 class GildedRoseTests: XCTestCase {
-    func testFoo() throws {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
-        let app = GildedRose(items: items)
-        app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
+    func testProperties() {
+        for _ in (0 ..< 10000) {
+            propertyTest()
+        }
     }
+
+    func propertyTest() {
+        let refItems = Array.items.randomElement()!
+        let sutItems = refItems.map { Item(name: $0.name, sellIn: $0.sellIn, quality: $0.quality) }
+
+        let ref = GildedRose(items: refItems)
+        ref.updateQuality()
+
+        let sut = GildedRose(items: refItems)
+        sut.updateQuality()
+
+        XCTAssertEqual(sut.items.count, ref.items.count)
+        XCTAssertEqual(sut.items.map(\.name), ref.items.map(\.name))
+        XCTAssertEqual(sut.items.map(\.sellIn), ref.items.map(\.sellIn))
+        XCTAssertEqual(sut.items.map(\.quality), ref.items.map(\.quality))
+    }
+}
+
+extension Array where Element == Item {
+    static let items = [
+        [],
+        [
+            Item(name: String.names.randomElement()!, sellIn: Int.sellIns.randomElement()!, quality: Int.qualities.randomElement()!)
+        ],
+        [
+            Item(name: String.names.randomElement()!, sellIn: Int.sellIns.randomElement()!, quality: Int.qualities.randomElement()!),
+            Item(name: String.names.randomElement()!, sellIn: Int.sellIns.randomElement()!, quality: Int.qualities.randomElement()!)
+        ],
+    ]
+}
+
+extension String {
+    static let names = [
+        "",
+        "Aged Brie",
+        "Backstage passes to a TAFKAL80ETC concert",
+        "Sulfuras, Hand of Ragnaros",
+    ]
+}
+
+extension Int {
+    static let sellIns = [
+        Int.min,
+        -1,
+        0,
+        1,
+        5,
+        6,
+        7,
+        10,
+        11,
+        12,
+        Int.max
+    ]
+
+    static let qualities = [
+        Int.min,
+        -1,
+        0,
+        1,
+        49,
+        50,
+        51,
+        Int.max
+    ]
 }
