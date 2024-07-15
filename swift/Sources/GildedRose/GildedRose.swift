ee6e8a95a6b03fa6d796940120aea274c69f55ca
Daniel Tartaglia <danielt1263@gmail.com>
2024-07-15 08:38:29 -0400

    Remove Aged Brie from quality switch.

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -1,17 +1,22 @@
 public class GildedRose {
     var items: [Item]
     private let updatedSellIns: [String: (Int) -> Int]
+    private let updatedQualities: [String: (Int, Int) -> Int]
+
     public init(items: [Item]) {
         self.items = items
         updatedSellIns = [
             "Sulfuras, Hand of Ragnaros": { $0 }
+        ]
+        updatedQualities = [
+            "Aged Brie": updatedAgedBrieQuality
         ]
     }
 
     public func updateQuality() {
         for each in items {
             let sellIn = (updatedSellIns[each.name] ?? updatedSellIn)(each.sellIn)
-            let quality = updatedQuality(name: each.name, sellIn: each.sellIn, quality: each.quality)
+            let quality = (updatedQualities[each.name] ?? { updatedQuality(name: each.name, sellIn: $0, quality: $1) })(each.sellIn, each.quality)
             each.sellIn = sellIn
             each.quality = quality
         }
@@ -22,15 +27,17 @@ func updatedSellIn(sellIn: Int) -> Int {
     sellIn - 1
 }
 
+func updatedAgedBrieQuality(sellIn: Int, quality: Int) -> Int {
+    guard quality < 50 else { return quality }
+    if sellIn > 0 {
+        return quality + 1
+    } else {
+        return min(quality + 2, 50)
+    }
+}
+
 func updatedQuality(name: String, sellIn: Int, quality: Int) -> Int {
     switch name {
-    case "Aged Brie":
-        guard quality < 50 else { return quality }
-        if sellIn > 0 {
-            return quality + 1
-        } else {
-            return min(quality + 2, 50)
-        }
     case "Backstage passes to a TAFKAL80ETC concert":
         guard sellIn > 0 else { return 0 }
         guard quality <= 50 else { return quality }
